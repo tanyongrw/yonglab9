cc5ca2dfb151f460a8473ea011890310ceb1a2a6
tanyongrw <rudjaya22934@gmail.com>
2022-12-23 17:00:42 +0700

    finish

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,56 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter your age: ";
+    cin >> a;
+    if(a<=25)
+    {
+    int b;
+    cout << "Enter your height: ";
+    cin >> b;
+        if(b<100)
+        {
+        cout << "Your character = Chopper";
+        return 0;
+        }
+        else if(b<180)
+        {
+        cout << "Your character = Usopp";
+        return 0;
+        }
+        else{
+        int c;
+        cout << "Enter your bounty: ";
+        cin >> c;
+        if(c>1100000000)
+        {
+        cout << "Your character = Zoro";
+        }
+        else
+        {
+         cout << "Your character = Sanji";
+        }}
+    }
+    else if(a<=60)
+    {
+    int e;
+        cout << "Enter your bounty: ";
+        cin >> e;
+    if(e>500000000)
+    {
+    cout << "Your character = Jinbe";
+    }
+    else
+    {
+    cout << "Your character = Franky";
+    }
+    }
+    else
+    {
+    cout << "Your character = Brook";
+}
+}
+
